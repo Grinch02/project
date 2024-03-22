bf2b38414f69e652c80d89b7bab3ec3b4f615e79
Julia <lulenochik@gmail.com>
2024-03-21 20:31:18 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+//  alert('hello');
+
+//const result = confirm('are you here?');
+//console.log(result); 
+
+// const answer = prompt('Вам есть 18?', '18');
+// console.log(answer);
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?','');
+// answers[1] = prompt('Как ваше фамилия?','');
+// answers[2] = prompt('Сколько вам лет?','');
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = "Ivan";
+
+alert(`Привет,${user}`); /* интерполяция (ES6) */
+
+
+  
